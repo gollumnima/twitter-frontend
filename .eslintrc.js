8f84fb08ff6c914ad7f2a0bdd46bc3078733783f
Doori Kim <doori.alice.kim@gmail.com>
2021-11-14 14:07:59 +0900

    chore: package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,44 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: { 
-    project: './tsconfig.json', 
-    sourceType: 'module' 
-  },
-  "env": {
-    "node": true, 
-    "browser": true,
-    "es2020": true,
-  },
-  plugins: ['react', '@typescript-eslint'],
   extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/eslint-recommended',
+    'airbnb',
+    'airbnb-typescript',
   ],
-  "rules": {
-    "react/jsx-filename-extension": [1, { "extensions": ["ts", "tsx"] }],
-    "arrow-parens": ["warn", "as-needed"],
-    "no-unused-vars": ["off"],
-    "no-console": ["off"],
-    "import/prefer-default-export": ["off"],
-    "react-hooks/exhaustive-deps": ["warn"],
-    "react/jsx-props-no-spreading": ["warn"],
-    "react/prop-types": ["off"],
-    'no-underscore-dangle': ['off'],
-    "indent": ['error', 2],
-  },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "moduleDirectory": ["node_modules", "."]
-      },
-      "typescript": {} // ts import issue fix
-    }
-  }
 };
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     project: './tsconfig.json',
+//     sourceType: 'module'
+//   },
+//   "env": {
+//     "node": true,
+//     "browser": true,
+//     "es2020": true,
+//   },
+//   plugins: ['react', '@typescript-eslint'],
+//   extends: [
+//     'airbnb-base',
+//     'plugin:@typescript-eslint/eslint-recommended',
+//   ],
+//   "rules": {
+//     "import/extensions": ["error", "never"],
+//     "react/jsx-filename-extension": [1, { "extensions": ["ts", "tsx"] }],
+//     "arrow-parens": ["warn", "as-needed"],
+//     "no-unused-vars": ["off"],
+//     "no-console": ["off"],
+//     "import/prefer-default-export": ["off"],
+//     "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+//     "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+//     "react/jsx-props-no-spreading": ["warn"],
+//     "react/prop-types": ["off"],
+//     'no-underscore-dangle': ['off'],
+//     "indent": ['error', 2],
+//   },
+//   "settings": {
+//     "import/resolver": {
+//       "node": {
+//         "moduleDirectory": ["node_modules", "."]
+//       },
+//       "typescript": {} // ts import issue fix
+//     }
+//   }
+// };
